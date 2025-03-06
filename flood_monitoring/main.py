aae03a26c7275a3a8d821e4dc1bfe013394dbc6a
Yusuf Sulehman <yusuf.sulehman@postgrad.manchester.ac.uk>
2025-03-06 19:53:36 +0000

    setting up project structure

---
+++ flood_monitoring/main.py
@@ -0,0 +1,8 @@
+import argparse
+from .monitor import FloodMonitor
+
+def main(args):
+    pass
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
---
+++ flood_monitoring/monitor.py
@@ -0,0 +1,5 @@
+class FloodMonitor:
+    # root path for the available APIs
+    root_pth = 'https://environment.data.gov.uk/flood-monitoring'
+    def __init__(self):
+        pass
